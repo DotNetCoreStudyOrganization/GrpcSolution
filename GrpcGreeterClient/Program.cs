c5d1321c63be72276ac7c4793c36bea0ef76fa8f
zouql <xx>
2019-08-13 17:25:09 +0800

    添加项目文件。

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GrpcGreeterClient
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Grpc.Net.Client;
+    using GrpcGreeter;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://localhost:5001";
+
+            var httpClient = new HttpClient
+            {
+                // The port number(5001) must match the port of the gRPC server.
+                BaseAddress = new Uri(url)
+            };
+
+            var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            Console.WriteLine("Press any key to exit...");
+
+            Console.ReadKey();
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+    }
+}
